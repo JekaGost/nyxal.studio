4bac6331341d656b05e1b9cabd5d320cab6b6fa5
Ievgenii Gostyshchev <jekagost@gmail.com>
2026-02-21 07:19:07 +0300

    pages added

--- lib/ui/pages/projects_page.dart
+++ lib/ui/pages/projects_page.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class ProjectsPage extends StatelessWidget {
+  const ProjectsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Наши Проекты', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
